cf6ffdce4a06d929084ba64f79d86c45f442b8e2
Andrew <andrew.blalock.ops@gmail.com>
2017-08-25 12:42:33 -0500

    Create nicks.js

---
+++ nicks.js
@@ -0,0 +1,14 @@
+var nicks = [
+  ["Name1", "Nickname1"],
+  ["Name2", "Nickname2"],
+  ["Name3", "Nickname3"]
+];
+
+document.addEventListener('DOMContentLoaded', function() {
+	$.each( nicks, function( key, value ) {
+		$("<script></script>").appendTo('head').html('$(document).ready(function(){ \
+			$("*").mouseover(function() { $(".p-channel_sidebar__name:contains(\'' + value[0] + '\')").each(function(){$(this).text("' + value[1] + '");}); }); \
+			$("*").mouseover(function() { $("a.message_sender:contains(\'' + value[0] + '\')").each(function(){$(this).text("' + value[1] + '");}); }); \
+			$("*").mouseover(function() { $("button#im_title:contains(\'' + value[0] + '\')").each(function(){$(this).text("' + value[1] + '");}); });});');
+	});
+});
