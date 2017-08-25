8925b42e3bcdd1df87c540d64eab2843fa865e91
Andrew <andrew.blalock.ops@gmail.com>
2017-08-25 14:04:33 -0500

    Update nicks.js

--- nicks.js
+++ nicks.js
@@ -7,7 +7,7 @@ var nicks = [
 document.addEventListener('DOMContentLoaded', function() {
 	$.each( nicks, function( key, value ) {
 		$("<script></script>").appendTo('head').html('$(document).ready(function(){ \
-			$("*").mouseover(function() { $(".p-channel_sidebar__name:contains(\'' + value[0] + '\'):not(:contains(\',\')), .p-channel_sidebar__name *:contains(\'' + value[0] + '\'):not(:contains(\',\'))").each(function(){$(this).text(\'' + value[1] + '\');});}); \
+			$("*").mouseover(function() { $(".p-channel_sidebar__name:contains(\'' + value[0] + '\'):not(:contains(\',\'))").each(function(){$(this).text(\'' + value[1] + '\');});}); \
 			$("*").mouseover(function() { $("a.message_sender:contains(\'' + value[0] + '\')").each(function(){$(this).text("' + value[1] + '");}); }); \
 			$("*").mouseover(function() { $("button#im_title:contains(\'' + value[0] + '\')").each(function(){$(this).text("' + value[1] + '");}); });});');
 	});
