5ec3fe715c44d344c5f990a132271655190f62c6
Andrew <andrew.blalock.ops@gmail.com>
2017-08-29 12:49:20 -0500

    Update nicks.js

--- nicks.js
+++ nicks.js
@@ -1,3 +1,4 @@
+//Nicknames plugin
 var nicks = [
   ["Name1", "Nickname1"],
   ["Name2", "Nickname2"],
@@ -5,11 +6,12 @@ var nicks = [
 ];
 
 document.addEventListener('DOMContentLoaded', function() {
-	$.each( nicks, function( key, value ) {
-		$("<script></script>").appendTo('head').html('$(document).ready(function(){ \
-			$("*").mouseover(function() { $(".p-channel_sidebar__name:contains(\'' + value[0] + '\'):not(:contains(\',\'))").each(function(){$(this).text(\'' + value[1] + '\');});}); \
-			$("*").mouseover(function() { $(".p-channel_sidebar__name:contains(\',\')").each(function(){$(this).css("color", "#5876a7");});}); \
-			$("*").mouseover(function() { $("a.message_sender:contains(\'' + value[0] + '\')").each(function(){$(this).text("' + value[1] + '");}); }); \
-			$("*").mouseover(function() { $("button#im_title:contains(\'' + value[0] + '\')").each(function(){$(this).text("' + value[1] + '");}); });});');
+	$("#msgs_div").bind("DOMNodeInserted", function(event){
+		$.each( nicks, function( key, value ) {
+			$(".p-channel_sidebar__name:contains(',')").each(function(){$(this).css("color", "#5876a7");});
+			$(".p-channel_sidebar__name:contains('" + value[0] + "'):not(:contains(','))").each(function(){$(this).text(value[1]);});
+			$("a.message_sender:contains('" + value[0] + "')").each(function(){$(this).text(value[1]);});
+			$("button#im_title:contains('" + value[0] + "')").each(function(){$(this).text(value[1]);});
+		});
 	});
 });
